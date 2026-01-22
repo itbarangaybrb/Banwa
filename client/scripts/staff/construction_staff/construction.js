89f1a43687534db3c466ceab35013b3e48381104
Jep0307 <muring.jeferson.ismael@gmail.com>
2026-01-23 00:50:26 +0800

    Revert "Merge branch 'jep' of https://github.com/Jep0307/Banwa into yddiebuls"
    This reverts commit b7e26f0a762c654af30c0e81cdc1ce814a974a50, reversing
    changes made to 0e57e66dc79e27009465c4a2426901fb5660764d.

--- client/scripts/staff/construction_staff/construction.js
+++ client/scripts/staff/construction_staff/construction.js
@@ -170,8 +170,7 @@ function loadAnalyticsTab() {
                         label: 'Construction Dates',
                         data: values1,
                         backgroundColor: dateColors,
-                        borderWidth: 1,
-                        tension: 0.4   
+                        borderWidth: 1
                     }]
                 },
                 options: {
