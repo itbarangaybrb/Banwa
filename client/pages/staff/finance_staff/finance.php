aae9079a1a5c4731baf1be2e137b795b57ad0868
deguzmanydlhazhirboctoy <deguzman.ydlhazhir.boctoy@gmail.comm>
2026-03-03 23:45:21 +0800

    ewan ko man

--- client/pages/staff/finance_staff/finance.php
+++ client/pages/staff/finance_staff/finance.php
@@ -298,6 +298,7 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <script>
         // Simple script to handle active class on sidebar items
