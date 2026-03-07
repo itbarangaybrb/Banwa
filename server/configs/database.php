3048cbf722cc4d366373d214556f0771dbc5dd87
Jep0307 <muring.jeferson.ismael@gmail.com>
2026-03-07 21:58:24 +0800

    minox fix, removed some comments

--- server/configs/database.php
+++ server/configs/database.php
@@ -1,19 +1,11 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-// Check if the file actually exists to debug the path
-$envPath = __DIR__ . '/../../';
-$envFile = $envPath . '.env';
-
-if (!file_exists($envFile)) {
-    die("ERROR: .env file not found at: " . realpath($envPath));
-}
-
-$dotenv = Dotenv\Dotenv::createImmutable($envPath);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->safeLoad();
 
-// Configuration - Define these FIRST
-$host = $_ENV['DB_HOST'] ?? 'host.docker.internal'; 
+// Configuration
+$host = $_ENV['DB_HOST'] ?? 'host.docker.internal'; // Default to the Docker bridge
 $db   = $_ENV['DB_NAME'] ?? 'capstone';
 $user = $_ENV['DB_USER'] ?? 'postgres';
 $pass = $_ENV['DB_PASS'] ?? '';
