d07107ef33f04b9534d6d97488205b2357fe436f
Jep0307 <muring.jeferson.ismael@gmail.com>
2026-03-10 19:19:19 +0800

    wip on .env

--- server/api/supabase.js
+++ server/api/supabase.js
@@ -2,7 +2,8 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 // import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 import dotenv from 'dotenv';
-dotenv.config();
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
