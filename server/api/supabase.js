30f7aee2595f1c4cb317cead26751737ca731478
Jep0307 <muring.jeferson.ismael@gmail.com>
2026-03-10 19:44:46 +0800

    wip on .env

--- server/api/supabase.js
+++ server/api/supabase.js
@@ -1,12 +1,8 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 // import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
-
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_KEY;
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+const SUPABASE_URL = 'https://owtynidzffffsiftqxbh.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93dHluaWR6ZmZmZnNpZnRxeGJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyMjE1NjMsImV4cCI6MjA3OTc5NzU2M30._Hbh-nCdTuVf6vuCb2Th2ruanFBF4wdZIVAVvrfLorY';
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY );
 
 export default supabase;
