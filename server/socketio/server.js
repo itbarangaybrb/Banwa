22fd0df56b7bd20db80b977692c74fde827e86f8
Jep0307 <muring.jeferson.ismael@gmail.com>
2026-07-22 18:22:16 +0800

    renewal: url and redirections

--- server/socketio/server.js
+++ server/socketio/server.js
@@ -7,7 +7,7 @@ const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
     cors: {
-        origin: 'https://banwa.onrender.com', // lock this down in production to your domain
+        origin: 'https://banwa-evw1.onrender.com', // lock this down in production to your domain
         methods: ['GET', 'POST']
     }
 });
